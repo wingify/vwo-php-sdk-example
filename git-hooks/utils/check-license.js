dc5fb76b46ea72d5e2959e21003f4f4d65075f37
softvar <varun2902@gmail.com>
2019-12-18 13:54:15 +0530

    fix(*): add copyright license in all src files

--- git-hooks/utils/check-license.js
+++ git-hooks/utils/check-license.js
@@ -50,7 +50,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -210,9 +210,9 @@ console.log(`${AnsiColorEnum.CYAN}\nVerifying License and Copyright${AnsiColorEn
 const isSuccess = checkLicenseUtil.checkLicenseAndCopyright({
   year: 2019,
   author: 'Wingify Software Pvt. Ltd.',
-  paths: 'src,tests',
+  paths: 'src',
   stoppingCriteria: '\\*\\/',
-  excludes: ['src/Error', 'src/Utils/Connection.php', 'src/Utils/murmur.php'],
+  excludes: [ 'src/templates/' ],
   extension: 'php'
 });
 console.timeEnd('Execution time for License and Copyright');
